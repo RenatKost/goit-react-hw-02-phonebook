73134aa6c9fa33f8a2c707845d1098d8ca85e992
RenatKost <renya.show@gmail.com>
2020-10-18 19:50:04 +0400

    Updates

---
+++ precache-manifest.8e5886564715d4b02b128d303f4b8fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77640c54c9088f3aaa5fa1dc5782d8ab",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "d7697624f17ca131d8c4",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.624e4e63.chunk.css"
+  },
+  {
+    "revision": "7755355ad166642a6773",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.75ecbc57.chunk.js"
+  },
+  {
+    "revision": "dfd9de20f4ff60fc8200b143aaf0a20e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.75ecbc57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7697624f17ca131d8c4",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.f9a75ea7.chunk.js"
+  },
+  {
+    "revision": "cc5d00c14a25a053fbb7",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.e35af0b9.js"
+  }
+]);
